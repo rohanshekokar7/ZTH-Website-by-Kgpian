e401ca4a830e0afa16e8ecfd5c50e560459a4543
Rohan Shekokar <shekokarrohan2007@gmail.com>
2026-05-28 18:42:32 +0530

    card

--- components/CurvedMarquee.tsx
+++ components/CurvedMarquee.tsx
@@ -62,7 +62,7 @@ export default function CurvedMarquee() {
           width: 100%;
           height: 120px; /* Reduced container height */
           overflow: hidden;
-          background: transparent;
+          background: #FFFFFF;
           display: flex;
           justify-content: center;
           align-items: center;
